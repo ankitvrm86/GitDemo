cdb5dd9c6e9bc69f9b97246abd369d7ffda1f7c4
ankit <ankitvrm86@gmail.com>
2025-04-06 12:17:07 +0530

    Asian Team commit

---
+++ src/main/java/GitX/App.java
@@ -0,0 +1,13 @@
+package GitX;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/GitX/Test1.java
@@ -0,0 +1,8 @@
+package GitX;
+
+public class Test1 {
+	public void TestCase1 () {
+		System.out.println("TestCase1");
+	}
+
+}
